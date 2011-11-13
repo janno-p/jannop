887340b0141a11e13ed0a7d3de164dd8b27449c9
Janno Põldma <janno.poldma@gmail.com>
2011-11-13 22:46:19 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jannop::Application.config.secret_token = '475ee5277accc9b31f3d167ef6460703d2db08c68956182d70739caf6cf419ab0cc2c137c5accc24b751c02bcd923bc114c8c07c72e8c7879619e456a2dc73cd'
