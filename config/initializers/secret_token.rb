9fec6c400c3a52ce08515a2497f0534bd7d605b7
Janno Põldma <janno.poldma@gmail.com>
2011-12-20 16:30:01 +0200

    Bundle & Rails upgrade

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Jannop::Application.config.secret_token = '475ee5277accc9b31f3d167ef6460703d2db08c68956182d70739caf6cf419ab0cc2c137c5accc24b751c02bcd923bc114c8c07c72e8c7879619e456a2dc73cd'
+Jannop::Application.config.secret_token = '6882831df4624d50822bffca6d8d2e9565e1e58e526a6a1b9e44384b016c3892b3344eb63976df7b6cce7a516dcdd9c3fa154e119733473b6150434a7ed8f9b1'
