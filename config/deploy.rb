f8374b41c9ecfa8be12a9aa6da600b0d6851d5db
Janno Põldma <janno.poldma@gmail.com>
2012-01-19 22:47:50 +0200

    Changed deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,62 +1,39 @@
-require 'FileUtils'
+# RVM bootstrap
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+set :rvm_ruby_string, '1.9.3-p0'
 
-dirname = File.dirname(__FILE__)
-filename = File.join(dirname, 'deploy_settings_local.yml')
+# Bundler bootstrap
+require 'bundler/capistrano'
 
-unless File.exists?(filename) then
-  raise "File deploy_settings_local.yml is missing..."
-end
+# Load sensitive info from settings file
+settings = YAML::load_file(File.join(File.dirname(__FILE__), 'deploy.yml'))
 
-settings = YAML::load_file(filename)
+# Main details
+set :application, settings["application"]
+role :web, settings["application"]
+role :app, settings["application"]
+role :db, settings["application"], :primary => true
 
-set :application, "jannop"
-set :deploy_to, settings["destination_path"]
-set :user, settings["server_username"]
-set :password, settings["server_password"]
-set :use_sudo, false
-
-set :scm, "git"
-set :scm_passphrase, settings["scm_passphrase"]
-set :repository, "git@github.com:janno-p/jannop.git"
-set :branch, "master"
-set :deploy_via, :remote_cache
-
-role :web, settings["web_server"]
-role :app, settings["app_server"]
-role :db, settings["db_server"], :primary => true
-
+# Server details
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
+set :deploy_to, settings["deploy_to"]
+set :deploy_via, :remote_cache
+set :user, settings["user"]
+set :use_sudo, false
 
-set :default_environment, {
-  'PATH'         => settings["rvm_path"],
-  'RUBY_VERSION' => settings["rvm_ruby_version"],
-  'GEM_HOME'     => settings["rvm_gem_home"],
-  'GEM_PATH'     => settings["rvm_get_path"],
-  'BUNDLE_PATH'  => settings["rvm_bundle_path"],
-}
+# Repository details
+set :scm, :git
+set :repository, "git@github.com:janno-p/jannop.git"
+set :branch, "master"
+set :git_enable_submodules, 1
 
+# Capistrano tasks
 namespace :deploy do
-  #after "deploy:setup", "deploy:jannop:setup"
-  #after "deploy:symlink", "deploy:jannop:symlink"
-  
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-  
-  namespace :jannop do
-    desc "Create required directories in shared folder"
-    task :setup do
-      run "cd #{shared_path}; mkdir ckeditor"
-      run "cd #{shared_path}; mkdir coins"
-    end
-    
-    desc "Create required symbolic links from shared to common folder"
-    task :symlink do
-      run "cd #{current_path}/public; rm -rf ckeditor_assets; ln -s #{shared_path}/ckeditor ckeditor_assets"
-      run "cd #{current_path}/public; rm -rf coins; ln -s #{shared_path}/coins coins"
-    end
   end
 end
