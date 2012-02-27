39031bf66740a438976d64338e2a3648b8436ade
Janno Põldma <janno.poldma@gmail.com>
2012-02-27 21:49:08 +0200

    Changed order in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,6 +50,6 @@ namespace :deploy do
 
   desc "reload the database with seed data"
   task :seed do
-    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; rake RAILS_ENV=#{rails_env} db:seed"
   end
 end
