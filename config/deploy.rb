305c9ff79fb1e60b86d654af527f04cc1cf6e6ac
Janno Põldma <janno.poldma@gmail.com>
2012-02-27 21:29:46 +0200

    Trying to fix update problem

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,9 +36,9 @@ set :git_enable_submodules, 1
 # Capistrano tasks
 namespace :deploy do
   after "deploy:symlink" do
-    run "cd #{current_path}/config; rm settings.yml; ln -sf #{shared_path}/settings.yml"
-    run "cd #{current_path}/db; rm production.sqlite3; ln -sf #{shared_path}/production.sqlite3"
-    run "cd #{current_path}/public; rm -f coins; ln -sf #{shared_path}/coins"
+    run "cd #{release_path}/config; rm settings.yml; ln -sf #{shared_path}/settings.yml"
+    run "cd #{release_path}/db; rm production.sqlite3; ln -sf #{shared_path}/production.sqlite3"
+    run "cd #{release_path}/public; rm -f coins; ln -sf #{shared_path}/coins"
   end
 
   task :start do ; end
